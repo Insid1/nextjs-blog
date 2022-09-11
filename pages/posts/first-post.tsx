bfdca1a53d644ae09bc7b2ec422d211090d3b42f
Kitty <kat.tarhanova@gmail.com>
2022-09-11 17:43:32 +0700

    finished style section

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,9 +1,18 @@
 import Link from 'next/link';
+import Head from "next/head";
+import Script from "next/script";
+import Layout from "../../components/layout";
 
 function FirstPost() {
-    return (
-        <Link href="/">to main page</Link>
-    );
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
+      <Link href="/">to main page</Link>
+    </Layout>
+  );
 }
 
 export default FirstPost;
